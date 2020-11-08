4ae5ee19953850eacfc5a504619657764df40845
Damian Goraj <damiangoraj@gmail.com>
2020-11-08 14:54:52 +0100

    dodatkowe wymaganie

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,23 +11,53 @@ namespace cw1
     {
         static async Task Main(string[] args)
         {
-            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
-            var client = new HttpClient();
-            var result = await client.GetAsync(url);
-
-            if (result.IsSuccessStatusCode)
+            //args = new string[] { "https://www.pja.edu.pl" }; //testing
+            var url = "";
+            if (args.Length > 0)
             {
-                string html = await result.Content.ReadAsStringAsync();
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]", RegexOptions.IgnoreCase);
-                var matches = regex.Matches(html);
-                foreach (var i in matches)
-                {
-                    Console.WriteLine(i);
-                }
+                url = args[0];
+            } else
+            {
+                throw new ArgumentNullException();
             }
+            var urlCorrectlyFormatted = Uri.IsWellFormedUriString(url, UriKind.Absolute);
+            if (!urlCorrectlyFormatted)
+            {
+                throw new ArgumentException();
+            }
+            var client = new HttpClient();
+            try
+            {
+                var result = await client.GetAsync(url);
 
-            Console.WriteLine("Hello World!");
+                if (result.IsSuccessStatusCode)
+                {
+                    string html = await result.Content.ReadAsStringAsync();
+                    var matches = Regex.Matches(html, "[a-z]+[a-z0-9]*@[a-z0-9]+(\\.[a-z]*){1,}", RegexOptions.IgnoreCase)
+                                        .OfType<Match>()
+                                        .Select(m => m.Groups[0].Value)
+                                        .Distinct();
+                    var emailsFound = matches.Count() > 0;
 
+                    if (emailsFound)
+                    {
+                        foreach (var i in matches)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    } else {
+                        Console.WriteLine("Nie znaleziono adresow email");
+                    }
+                }
+           
+                
+            } catch (Exception e)
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            } finally
+            {
+                client.Dispose();
+            }
         }
     }
 }
